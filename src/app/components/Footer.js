f3504e047e15969c941938e88ce9f9b01270834f
Gerald Sheenan <sheenangerald@gmail.com>
2025-10-19 11:40:53 +0700

    initial setup

---
+++ src/app/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className='text-sm text-center italic'>
+            <p>&copy; 2025 CuyFace. Ramalan ini berdasarkan model AI vision, bukan untuk sepenuhnya dipercaya.</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/app/components/Navheader.js
@@ -0,0 +1,10 @@
+function Navheader() {
+    return (
+        <header className="text-center">
+            <h1 className="text-4xl font-extrabold bg-gradient-to-r from-cyan-300 via-green-400 to-blue-500 text-transparent bg-clip-text shadow-lg inline-block" style={{ filter: "drop-shadow(0 0 10px rgba(235, 70, 150, 0.7))" }}> CUYFACE | PREDICT YOUR FACE </h1>
+            <p className="">Ramal berdasarkan foto selfie kalian</p>
+        </header>
+    );
+}
+
+export default Navheader;
